56b98ce3a206bc9ff53afe1f76c3bce8b5aceecf
110@qq.com <110@qq.com>
2019-01-28 22:35:25 +0800

    2

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 
 from setuptools import find_packages, setup
 setup(
-    name='KLineAnalysis',
+    name='KLineAnalys',
     version='0.0.1.1',
     description='this is a Finance Analysis System',
-    author='fdsn',#作者
-    author_email='670428704@qq.com',
+    author='fdsn670428704',#作者
+    author_email="670428704@qq.com",
     url='https://github.com/670428704/FinanceAnalysis.git',
     # packages=find_packages(),
-    packages=['KLineAnalysis'],  #这里是所有代码所在的文件夹名称
-    install_requires=['requests'],
+    packages=find_packages(),  #这里是所有代码所在的文件夹名称
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
